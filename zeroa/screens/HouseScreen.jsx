d9f8ed7e26619fbc2b3bfe815192dcf7e721e86e
lloyd.probook450gb <kayembeloyd@gmail.com>
2023-12-12 17:48:51 +0200

    Screens setup

---
+++ zeroa/screens/HouseScreen.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HouseScreen() {
+  return (
+    <View>
+      <Text>House Screen</Text>
+    </View>
+  );
+}
